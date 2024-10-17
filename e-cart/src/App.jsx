6b68872c7b275dfc864091925a2d52e8dad69102
joyal james <joyaljames58@gmail.com>
2024-10-17 09:57:25 +0530

    ecart

---
+++ e-cart/src/App.jsx
@@ -0,0 +1,31 @@
+
+import { Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+import Footer from './Components/Footer'
+import View from './Pages/View'
+import Cart from './Pages/Cart'
+import Home from './Pages/Home'
+import Wishlist from './Pages/Wishlist'
+
+
+function App() {
+
+  return (
+    <>
+    
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/Wishlist' element={<Wishlist/>}/>
+    <Route path='/Cart' element={<Cart/>}/>
+    <Route path='/View/:id' element={<View/>}/>
+    {/* {redirect to home} */}
+    <Route path='/*' element={<Navigate to={'/'}/> }/>
+    </Routes>
+    <Footer/>
+     
+    </>
+  )
+}
+
+export default App
